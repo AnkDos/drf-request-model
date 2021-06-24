8fc7325319bcf452ae7ca5295d6faad31a53d574
Ankur <ankurpan96@gmail.com>
2021-06-24 12:39:10 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -16,16 +16,19 @@ query_param_model_example = {
 simple_json_model_example = {
     "name": RequestModel(data_type=str, regex="^[a-zA-Z][a-zA-Z\' .,-]+[a-zA-Z]$"),
     "marks": RequestModel(data_type=list, required=False),
-    "address": RequestModel(data_type=list)
+    "address": RequestModel(data_type=list),
+    "int": RequestModel(data_type=int)
 }
 
 nested_json_model_example = {
     "name": RequestModel(data_type=str, regex="^[a-zA-Z][a-zA-Z\' .,-]+[a-zA-Z]$"),
     "marks": RequestModel(data_type=list, required=False),
     "address": RequestModel(data_type=dict, 
-                                    nested={"street":RequestModel(data_type=str),
+                                    nested={"street":RequestModel(data_type=int),
                                             "pincodes":  RequestModel(data_type=list),
-                                            "corraddr": RequestModel(data_type=dict, nested={"street2": RequestModel(data_type=str , nested={"ank": RequestModel(data_type=int)})})
+                                            "corraddr": RequestModel(data_type=dict, nested={"street": RequestModel(data_type=str) ,"ank": RequestModel(data_type=int)})
                         
-                                            })
+                                            }),
+    "nk": RequestModel(data_type=str),
+    "ta": RequestModel(data_type=dict, nested={"cool": RequestModel(data_type=bool) ,"aa": RequestModel(data_type=list)})
 }
